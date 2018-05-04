5c90437e3a171311244fab3971d12047376119a9
rizadhani <rizadhani@users.noreply.github.com>
2018-05-04 16:26:56 +0800

    Instance Object

--- src/buriri01/GadgetAksi.java
+++ src/buriri01/GadgetAksi.java
@@ -12,13 +12,6 @@ package buriri01;
 public class GadgetAksi {
     public static void main(String[] args) {
         Gadget Notebook = new Gadget();
-        
-        Notebook.merk="Acer";
-        Notebook.warna="Hitam";
-        Notebook.tahun="2016";
-        Notebook.ukuran="10 inci";
-        Notebook.harga="3Juta";
-        
-        Notebook.cetakInfo();
+
     }
 }
